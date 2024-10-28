7b477f12ae2fa5a7b63ebcd33db95f804f19dc5a
ManojNambiraj <postboxmano@gmail.com>
2024-10-28 20:32:23 +0530

    Class 2 useStates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import Btn from "./Btn";
+// import Btn from "./Btn";
 import "./App.css"
+import { useState } from "react";
 
 function App() {
-  const handleClick = (value) => {
-    alert(value);
-  };
+
+  const [state, setState] = useState(0);
+  const [color, setColor] = useState("white")
+
+  const handleClick = () => {
+    setState(state + 1)
+    setColor("green")
+  }
+
+  const handleDislike = () => {
+    if(state === 0){
+      setState(0)
+    }else{
+      setState(state - 1)
+    }
+    
+    setColor("white")
+  }
 
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="App" style={{backgroundColor: color}}>
+      <h1>{state}</h1>
 
-      <p>Good Evening...!</p>
-
-      <Btn username={"just"} handleClick={handleClick} />
-      <Btn username={"All"} handleClick={handleClick} />
-      <Btn username={"Movies"} handleClick={handleClick} />
-      <Btn username={"Cars"} handleClick={handleClick} />
-      <Btn username={"Cycles"} handleClick={handleClick} />
+      <button onClick={handleClick}>Like 👍</button>
+      <button onClick={handleDislike}>Dislike 👎</button>
     </div>
-  );
+  )
+
+
+
+
+
+
+
+  // const handleClick = (value) => {
+  //   alert(value);
+  // };
+
+  // return (
+  //   <div className="App">
+  //     <h1>Hello World</h1>
+
+  //     <p>Good Evening...!</p>
+
+  //     <Btn username={"just"} handleClick={handleClick} />
+  //     <Btn username={"All"} handleClick={handleClick} />
+  //     <Btn username={"Movies"} handleClick={handleClick} />
+  //     <Btn username={"Cars"} handleClick={handleClick} />
+  //     <Btn username={"Cycles"} handleClick={handleClick} />
+  //   </div>
+  // );
 }
 
 export default App;
+
+// Hooks  -----> useState
